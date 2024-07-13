29a0a3150f4daa2bdd553f48dc9b0e11e21b171f
vitor amorim <vitoralmada10@gmail.com>
2024-07-12 23:13:38 -0300

    entrega desafio 2 09 e 11/07

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+    def ask
+    end
+
+    def answer
+        @question = params[:question]
+        @answer = resposta(@question)
+    end
+
+    def resposta(question)
+        if question == "I am going to work" 
+            "Great!"
+        elsif question.end_with?("?")
+            "Silly question, get dressed and go to work!"
+        else
+            "I don't care, get dressed and go to work!"
+        end
+    end
+end
